d9ef7b08eb792fe16b52ba02b219b98eb319334a
Chakshu Gupta <chakshugupta2000@gmail.com>
2019-08-20 22:15:55 +0530

    add: vote page

--- main-server/main-server.py
+++ main-server/main-server.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template
 import sqlite3
 import requests
-import json
+
 app = Flask(__name__, static_url_path='', static_folder='static')
 
 
@@ -43,26 +43,27 @@ def get():
     return render_template('index.html')
 
 
-# @app.route("/chart", methods=["GET"])
-# def chart():
-#     with sqlite3.connect("earthquake_data.db") as con:
-#         cur = con.cursor()
-#         cur.execute("SELECT * FROM Acceleration ")
-#         rows = cur.fetchall()
-#         print(rows)
-#         labels = []
-#         values = []
-#         for row in rows:
-#             values = values + [row[0], ]
-#             labels = labels + [row[3], ]
-#     return render_template('chart.html', values=values, labels=labels)
+@app.route("/chart", methods=["GET"])
+def chart():
+    #     with sqlite3.connect("earthquake_data.db") as con:
+    #         cur = con.cursor()
+    #         cur.execute("SELECT * FROM Acceleration ")
+    #         rows = cur.fetchall()
+    #         print(rows)
+    #         labels = []
+    #         values = []
+    #         for row in rows:
+    #             values = values + [row[0], ]
+    #             labels = labels + [row[3], ]
+    return render_template('chart.html', )
+
 
 url = "https://cfd-7lizlp-api.azurewebsites.net/api/v1/contracts/8"
 
 payload = ""
 headers = {
     'Content-Type': "application/json",
-    'Authorization': "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6ImllX3FXQ1hoWHh0MXpJRXN1NGM3YWNRVkduNCIsImtpZCI6ImllX3FXQ1hoWHh0MXpJRXN1NGM3YWNRVkduNCJ9.eyJhdWQiOiI1MWMxODZkNy04ODViLTQxNDMtYWZhYS0yODc1OTZkNjg2OGUiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC8wODNiNmQ1Mi0zYzQ3LTRhMGYtOTg2ZC0zNmZkYjBkOWI0YWUvIiwiaWF0IjoxNTY2MzA1MjM0LCJuYmYiOjE1NjYzMDUyMzQsImV4cCI6MTU2NjMwOTEzNCwiYWlvIjoiQVZRQXEvOE1BQUFBeTQ0QzBkQXRBNmt0eGo4OXBYYUtNWGtHYVJaVE50c1BzdHVKWGhJaGFMdHFzcUF3TWhkRnp5RnM3YTNjUVlkQzduNDNDTVU0RmtyRk1saW90WGJpQXdaeDZ6Z0JnZDJ4dURZUDVFa1ZsbFk9IiwiYW1yIjpbInB3ZCJdLCJjX2hhc2giOiJNZXlQaGNmSGVrNTFTOUhyankyUjZ3IiwiZW1haWwiOiJhaHVqYS4yQGlpdGouYWMuaW4iLCJmYW1pbHlfbmFtZSI6ImU2YTkzODgxLWUzZTctNGVmOC05YTE2LWFiOTE3MjhiNTdmYiIsImdpdmVuX25hbWUiOiI5MjdhNjRlZS0wNDQ0LTQ2ODEtYWVjZC1hNGE0YjI3ZTc5ZTkiLCJpZHAiOiJsaXZlLmNvbSIsImlwYWRkciI6IjE0LjEzOS4zNy4xMDgiLCJuYW1lIjoiOTI3YTY0ZWUtMDQ0NC00NjgxLWFlY2QtYTRhNGIyN2U3OWU5IGU2YTkzODgxLWUzZTctNGVmOC05YTE2LWFiOTE3MjhiNTdmYiIsIm5vbmNlIjoiMjRhMTVmYmItMTMzNi00YWIyLTk1ODQtNzM4MGRhODIwOTJiIiwib2lkIjoiZTQ1OWUzOTktNWVlZS00YTY3LTllZGMtZjY1MTIwMWRlYTc2Iiwicm9sZXMiOlsiQWRtaW5pc3RyYXRvciJdLCJzdWIiOiJkSmJOMWU1R1JhMzFiREpsT1dLT25vcUljV3dKUHVEYWdJMDFzWTRNR25vIiwidGlkIjoiMDgzYjZkNTItM2M0Ny00YTBmLTk4NmQtMzZmZGIwZDliNGFlIiwidW5pcXVlX25hbWUiOiJsaXZlLmNvbSNhaHVqYS4yQGlpdGouYWMuaW4iLCJ1dGkiOiJrc2cwMzZTOTRFU044dGtDUENoSUFBIiwidmVyIjoiMS4wIn0.CJPIp8xDVlS2PoM-M14g4XfmdxoEC4OnHVerJzX8aw5U1YzAWeZepUJW8YeggFzwq-V2XLfeIM07AGJy-gdoS2ald88kFvLFFruJ78Thb9O16BQ3WMnr-3wCtwUe8dtfDCo6WhnRe5rN-ZAMzIYEutCuAL9y5EHw1PgRMeNsVleGkNH0JI-IrVJJl2aUteXH41MCkbc9w4E_3-J1tM1_LIaSZ6Kt6Ij08w1FMW8t26my_5vHLO8qj6duIgfnEleCmuvr-bwDGMJJa0kyGCRYSRTF0YpC-EBNiQbpJ3e7cjLObVDpCubXDm8M741VirizKSeq5M7bM62MFDEoMUnQoQ",
+    'Authorization': "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6ImllX3FXQ1hoWHh0MXpJRXN1NGM3YWNRVkduNCIsImtpZCI6ImllX3FXQ1hoWHh0MXpJRXN1NGM3YWNRVkduNCJ9.eyJhdWQiOiI1MWMxODZkNy04ODViLTQxNDMtYWZhYS0yODc1OTZkNjg2OGUiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC8wODNiNmQ1Mi0zYzQ3LTRhMGYtOTg2ZC0zNmZkYjBkOWI0YWUvIiwiaWF0IjoxNTY2MzE2MzE5LCJuYmYiOjE1NjYzMTYzMTksImV4cCI6MTU2NjMyMDIxOSwiYWlvIjoiQVZRQXEvOE1BQUFBdWZaazJ3TTV3eDh4bEY5cis5cXN5T3B4WS9LODdmYmpoSU90alYzL0NZenVrQnhtcE9nSWZBZCtwRmtyckdHeWVHQ21QSG85a1ZVY0RjbzlTUWpEVlpuSEx4UmxORlZYeWhLZnpTTmZZTGc9IiwiYW1yIjpbInB3ZCJdLCJjX2hhc2giOiJUVDFHYTFPdHBwNFdMWjcxWE5CclZnIiwiZW1haWwiOiJhaHVqYS4yQGlpdGouYWMuaW4iLCJmYW1pbHlfbmFtZSI6ImU2YTkzODgxLWUzZTctNGVmOC05YTE2LWFiOTE3MjhiNTdmYiIsImdpdmVuX25hbWUiOiI5MjdhNjRlZS0wNDQ0LTQ2ODEtYWVjZC1hNGE0YjI3ZTc5ZTkiLCJpZHAiOiJsaXZlLmNvbSIsImlwYWRkciI6IjE0LjEzOS4zNy4xMDgiLCJuYW1lIjoiOTI3YTY0ZWUtMDQ0NC00NjgxLWFlY2QtYTRhNGIyN2U3OWU5IGU2YTkzODgxLWUzZTctNGVmOC05YTE2LWFiOTE3MjhiNTdmYiIsIm5vbmNlIjoiZWMxZjRlMmQtNWIzYS00ZGQ3LWIxNDQtNTFkNDY4ZTY3MDMzIiwib2lkIjoiZTQ1OWUzOTktNWVlZS00YTY3LTllZGMtZjY1MTIwMWRlYTc2Iiwicm9sZXMiOlsiQWRtaW5pc3RyYXRvciJdLCJzdWIiOiJkSmJOMWU1R1JhMzFiREpsT1dLT25vcUljV3dKUHVEYWdJMDFzWTRNR25vIiwidGlkIjoiMDgzYjZkNTItM2M0Ny00YTBmLTk4NmQtMzZmZGIwZDliNGFlIiwidW5pcXVlX25hbWUiOiJsaXZlLmNvbSNhaHVqYS4yQGlpdGouYWMuaW4iLCJ1dGkiOiJWeVNyd2s1VldVR3hhcnZfODBIN0FBIiwidmVyIjoiMS4wIn0.PjJc76YpOEH5iAn38fxbW35RlJ8PZmMCKTD5Zox7Son7kb4ZtMiivYTxIlr4LRzYDr8EqUFLbqGkgM2g2Uyl5y1CV7adCzkoqtaV1R3r_WJdq9gbJlg2_P1r1t3-tPBogWiAZ6mlxhKbZnh16mxWNEG5CVYhmAopjC7vDjnqWEPsQBbWiIHIwJXCjQCKoiO7U8V-pqBXyzJos8zGByXZb-c1wn_AqvrV7BEY4OxGP3kTKNL_2NWdgwPCsO0WgMaE8BiNZfZp4BOEQ5_XoHiXqVKG9OlOpcBfmD92n682gu4lTmYHnc-ihvmDa4FKFU22jZEGdDUFIyImLthRp9Mkgw",
     'User-Agent': "PostmanRuntime/7.15.2",
     'Accept': "*/*",
     'Cache-Control': "no-cache",
@@ -75,17 +76,17 @@ headers = {
 }
 
 
-@app.route("/candidates", methods=["GET"])
-def candidates():
-    response = requests.request("GET", url, data=payload, headers=headers)
-    response = response.json()
-    x = response["contractActions"]
-    candidateID = []
-    for k in range(len(x)):
-        z = x[k]
-        if z['userId'] == 1:
-            candidateID.append(z['parameters'][0]['value'])
-    return render_template('candidate.html', candidateID=candidateID)
+# @app.route("/candidates", methods=["GET"])
+# def candidates():
+#     response = requests.request("GET", url, data=payload, headers=headers)
+#     response = response.json()
+#     x = response["contractActions"]
+#     candidateID = []
+#     for k in range(len(x)):
+#         z = x[k]
+#         if z['userId'] == 1:
+#             candidateID.append(z['parameters'][0]['value'])
+#     return render_template('candidate.html', candidateID=candidateID)
 
 
 @app.route("/votes", methods=["GET"])
@@ -93,29 +94,27 @@ def votes():
     response = requests.request("GET", url, data=payload, headers=headers)
     response = response.json()
     x = response["contractActions"]
-    candidateID = {}
-    total = 0
+    candidate_id = {}
+    total_votes = 0
     for k in range(len(x)):
         z = x[k]
         if z['userId'] == 1:
-            candidateID[(z['parameters'][0]['value'])] = 0
+            if len(z['parameters'][0]['value']) < 32:
+                candidate_id[(z['parameters'][0]['value'])] = 0
 
     for kk in range(len(x)):
+        z = x[kk]
         if z['userId'] == 3:
-            candidateID[(z['parameters'][0]['value'])] += 1
-            total += 1
-    return render_template('vote.html', candidateID=candidateID, total=total)
+            if len(z['parameters'][0]['value']) < 32:
+                candidate_id[(z['parameters'][0]['value'])] += 1
+                total_votes += 1
 
-
-@app.route("/info", methods=["GET"])
-def info():
     with sqlite3.connect("candidateinfo.db") as con:
         cur = con.cursor()
         cur.execute("SELECT * FROM Info")
         rows = cur.fetchall()
 
-    return render_template('info.html', candidateInfo=rows)
-
+    return render_template('votes.html', candidateID=candidate_id, total=total_votes, candidateInfo=rows)
 
 # @app.route("/maps", methods=["GET"])
 # def maps():
